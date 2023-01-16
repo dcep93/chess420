645ff6c318bdd17a89e2b2d53925bcfc9dea805c
dcep93 <dcep93@gmail.com>
2023-01-16 13:59:42 -0500

    x

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -9,7 +9,7 @@ SA_KEY="$1"
 # set -e
 # nvm install 16.4.0
 # firebase projects:addfirebase $GOOGLE_CLOUD_PROJECT
-# firebase init hosting --project "$GOOGLE_CLOUD_PROJECT"
+# yes "" | firebase init hosting --project "$GOOGLE_CLOUD_PROJECT"
 # gcloud iam service-accounts create deployer-github
 # gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/firebasehosting.admin"
 # gcloud iam service-accounts keys create gac.json --iam-account "deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
