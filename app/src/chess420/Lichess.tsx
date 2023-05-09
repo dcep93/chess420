86136ec56893557493ce977a95a7a6f4df1eb804
dcep93 <dcep93@gmail.com>
2023-05-08 19:42:01 -0700

    x

--- app/src/chess420/Lichess.tsx
+++ app/src/chess420/Lichess.tsx
@@ -27,55 +27,57 @@ export default function lichess(
   attempt: number = 1,
   ratings: number[] = [2000, 2200, 2500]
 ): Promise<LiMove[]> {
-  return helper(chess, isOriginal, attempt, ratings).then((moves) =>
-    moves.map((move: LiMove) => ({
-      ...move,
-      score: score(chess, move),
-      total: move.black + move.white + move.draws,
-    }))
-  );
+  return helper(chess, attempt, ratings)
+    .then((moves) =>
+      moves.map((move: LiMove) => ({
+        ...move,
+        score: score(chess, move),
+        total: move.black + move.white + move.draws,
+      }))
+    )
+    .then((moves) => {
+      const total = moves
+        .map((move: LiMove) => move.total)
+        .reduce((a: number, b: number) => a + b, 0);
+      if (isOriginal)
+        setTimeout(() =>
+          moves
+            .filter((move: LiMove) => move.total >= total * 0.01)
+            .forEach((move: LiMove) => {
+              const subChess = Brain.getChess();
+              subChess.load(chess.fen());
+              subChess.move(move.san);
+              lichess(subChess, false, attempt + 1);
+            })
+        );
+      return moves;
+    });
 }
 
 async function helper(
   chess: ChessInstance,
-  isOriginal: boolean,
   attempt: number,
   ratings: number[]
 ): Promise<any[]> {
+  if (attempt > 10) return [];
   const url = `https://explorer.lichess.ovh/lichess?variant=standard&speeds=rapid,classical&ratings=${ratings.join(
     ","
   )}&fen=${chess.fen()}`;
   const storedMoves = StorageW.get(url);
   if (storedMoves !== null) return Promise.resolve(storedMoves.moves);
 
-  console.log("fetching", attempt, url);
+  console.log("fetching", attempt, chess.fen());
   const response = await fetch(url);
   if (!response.ok)
     return new Promise((resolve, reject) =>
       setTimeout(
         () =>
-          helper(chess, isOriginal, attempt + 1, ratings).then((moves) =>
-            resolve(moves)
-          ),
+          helper(chess, attempt + 1, ratings).then((moves) => resolve(moves)),
         1000
       )
     );
   const json = await response.json();
   const moves = json.moves;
   StorageW.set(url, json);
-  const total = moves
-    .map((move: LiMove) => move.total)
-    .reduce((a: number, b: number) => a + b, 0);
-  if (isOriginal)
-    setTimeout(() =>
-      moves
-        .filter((move: LiMove) => move.total >= total * 0.01)
-        .forEach((move: LiMove) => {
-          const subChess = Brain.getChess();
-          subChess.load(chess.fen());
-          subChess.move(move.san);
-          lichess(subChess, false, attempt + 1);
-        })
-    );
   return moves;
 }
--- app/src/chess420/StorageW.tsx
+++ app/src/chess420/StorageW.tsx
@@ -1,7 +1,17 @@
+const version = "0.0.1";
+
 export default class StorageW {
   static get(key: string): any {
-    return null;
+    const raw = localStorage.getItem(getKey(key));
+    if (raw === null) return null;
+    return JSON.parse(raw);
   }
 
-  static set(key: string, content: any) {}
+  static set(key: string, content: any) {
+    localStorage.setItem(getKey(key), JSON.stringify(content));
+  }
+}
+
+function getKey(key: string) {
+  return `${version}/${key}`;
 }
