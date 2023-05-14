f42892d76fe0c25d4dfc83c0a0c8b6a92c04b7ba
dcep93 <dcep93@gmail.com>
2023-05-14 18:05:27 -0400

    x

--- app/src/chess420/Brain.tsx
+++ app/src/chess420/Brain.tsx
@@ -84,6 +84,10 @@ export default class Brain {
       index: 0,
       states,
     });
+    Brain.maybeReply(state);
+  }
+
+  static maybeReply(state: StateType) {
     if (
       (!Brain.autoreplyRef.current || Brain.autoreplyRef.current!.checked) &&
       state.chess.turn() === (state.orientationIsWhite ? "b" : "w")
@@ -144,7 +148,7 @@ export default class Brain {
   }
 
   static playWeighted() {
-    lichess(Brain.getState().chess, true)
+    lichess(Brain.getState().chess, { prepareNext: true })
       .then((moves) => {
         const weights = moves.map((move: LiMove) => Math.pow(move.total, 1.5));
         var choice = Math.random() * weights.reduce((a, b) => a + b, 0);
@@ -164,7 +168,7 @@ export default class Brain {
         return Brain.playMove(novelty);
       }
     }
-    lichess(Brain.getState().chess, true)
+    lichess(Brain.getState().chess, { prepareNext: true })
       .then((moves) => moves.sort((a, b) => b.score - a.score))
       .then((moves) => moves[0]?.san)
       .then((san) => san && Brain.playMove(san));
--- app/src/chess420/Lichess.tsx
+++ app/src/chess420/Lichess.tsx
@@ -13,6 +13,82 @@ export type LiMove = {
   score: number;
 };
 
+const promises: { [key: string]: Promise<LiMove[]> } = {};
+type OptionsType = {
+  username?: string;
+  prepareNext?: boolean;
+  attempt?: number;
+};
+
+export default function lichess(
+  chess: ChessInstance,
+  options: OptionsType = {}
+): Promise<LiMove[]> {
+  const prepareNext = options.prepareNext || false;
+  const attempt = options.attempt || 0;
+
+  const ratings = [2000, 2200, 2500];
+  const url = `https://explorer.lichess.ovh/lichess?variant=standard&speeds=rapid,classical&ratings=${ratings.join(
+    ","
+  )}&fen=${chess.fen()}`;
+  const key = JSON.stringify({
+    url,
+  });
+  const pp = promises[key];
+  if (pp) {
+    return pp;
+  }
+  const p = helper(url, attempt)
+    .then((moves) =>
+      moves
+        .map((move: LiMove) => ({
+          ...move,
+          total: move.black + move.white + move.draws,
+        }))
+        .map((move: LiMove) =>
+          getScore(chess, move).then((score) => ({ ...move, score }))
+        )
+    )
+    .then((movePromises: Promise<LiMove>[]) => Promise.all(movePromises))
+    .then((moves: LiMove[]) => {
+      const total = moves
+        .map((move: LiMove) => move.total)
+        .reduce((a: number, b: number) => a + b, 0);
+      if (prepareNext)
+        setTimeout(() =>
+          moves
+            .filter((move: LiMove) => move.total >= total * 0.01)
+            .forEach((move: LiMove) => {
+              const subChess = Brain.getChess(chess, [move.san]);
+              lichess(subChess, { prepareNext: false, attempt: attempt + 1 });
+            })
+        );
+      return moves;
+    });
+  promises[key] = p;
+  return p;
+}
+
+async function helper(url: string, attempt: number): Promise<any[]> {
+  if (attempt > 10) return [];
+  const storedMoves = StorageW.get(url);
+  if (storedMoves !== null) return Promise.resolve(storedMoves.moves);
+
+  console.log("fetching", attempt, url);
+  const response = await fetch(url);
+  if (!response.ok)
+    return new Promise((resolve, reject) =>
+      setTimeout(
+        () => helper(url, attempt + 1).then((moves) => resolve(moves)),
+        1000
+      )
+    );
+  const json = await response.json();
+  const moves = json.moves;
+  StorageW.set(url, json);
+  return moves;
+}
+
 function getScore(chess: ChessInstance, move: LiMove): Promise<number> {
   const isWhite = chess.turn() === "w";
   const p =
@@ -48,74 +124,3 @@ function getScore(chess: ChessInstance, move: LiMove): Promise<number> {
 //       return bestMove.total;
 //     });
 // }
-
-const promises: { [key: string]: Promise<LiMove[]> } = {};
-
-export default function lichess(
-  chess: ChessInstance,
-  prepareNext: boolean = false,
-  attempt: number = 1
-): Promise<LiMove[]> {
-  const key = JSON.stringify({
-    chess: chess.fen(),
-    prepareNext,
-    attempt,
-  });
-  const pp = promises[key];
-  if (pp) {
-    return pp;
-  }
-  const p = helper(chess, attempt)
-    .then((moves) =>
-      moves
-        .map((move: LiMove) => ({
-          ...move,
-          total: move.black + move.white + move.draws,
-        }))
-        .map((move: LiMove) =>
-          getScore(chess, move).then((score) => ({ ...move, score }))
-        )
-    )
-    .then((movePromises: Promise<LiMove>[]) => Promise.all(movePromises))
-    .then((moves: LiMove[]) => {
-      const total = moves
-        .map((move: LiMove) => move.total)
-        .reduce((a: number, b: number) => a + b, 0);
-      if (prepareNext)
-        setTimeout(() =>
-          moves
-            .filter((move: LiMove) => move.total >= total * 0.01)
-            .forEach((move: LiMove) => {
-              const subChess = Brain.getChess(chess, [move.san]);
-              lichess(subChess, false, attempt + 1);
-            })
-        );
-      return moves;
-    });
-  promises[key] = p;
-  return p;
-}
-
-async function helper(chess: ChessInstance, attempt: number): Promise<any[]> {
-  const ratings = [2000, 2200, 2500];
-  if (attempt > 10) return [];
-  const url = `https://explorer.lichess.ovh/lichess?variant=standard&speeds=rapid,classical&ratings=${ratings.join(
-    ","
-  )}&fen=${chess.fen()}`;
-  const storedMoves = StorageW.get(url);
-  if (storedMoves !== null) return Promise.resolve(storedMoves.moves);
-
-  console.log("fetching", attempt, chess.fen());
-  const response = await fetch(url);
-  if (!response.ok)
-    return new Promise((resolve, reject) =>
-      setTimeout(
-        () => helper(chess, attempt + 1).then((moves) => resolve(moves)),
-        1000
-      )
-    );
-  const json = await response.json();
-  const moves = json.moves;
-  StorageW.set(url, json);
-  return moves;
-}
