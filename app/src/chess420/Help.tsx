389551c02868426e2075d5d3c1ca7fc827578d62
dcep93 <dcep93@gmail.com>
2023-06-19 14:50:06 -0400

    x

--- app/src/chess420/Help.tsx
+++ app/src/chess420/Help.tsx
@@ -1,3 +1,20 @@
+import BrainC from "./BrainC";
+
 export default function Help() {
-  return null; // TODO
+  return (
+    <div
+      onClick={() => BrainC.updateShowHelp(false)}
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        overflow: "scroll",
+        zIndex: 1,
+        padding: "2em",
+        cursor: "pointer",
+      }}
+    >
+      <h1>welcome to chess420</h1>
+    </div>
+  );
 }
