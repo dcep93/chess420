714035ef0b9d8cfd339a47e05fa376d5d89362f7
dcep93 <dcep93@gmail.com>
2025-02-02 12:00:02 -0500

    x

--- app/src/chess420/index.tsx
+++ app/src/chess420/index.tsx
@@ -102,6 +102,7 @@ function SubMain(props: { fen: string }) {
   // TODO mobile friendly
   if (
     settings.SHOULD_UPDATE_HASH &&
+    Brain.view !== View.lichess_id &&
     Brain.view !== View.lichess_latest &&
     Brain.view !== View.lichess_mistakes &&
     Brain.view !== View.traverse
