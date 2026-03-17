752ad4099c1449b6ebb8ee2f864d00498612a2a8
Daniel Cepeda <dcep93@gmail.com>
2026-03-17 16:05:22 -0400

    x

--- app/src/chess420/Controls.tsx
+++ app/src/chess420/Controls.tsx
@@ -88,6 +88,7 @@ export default function Controls() {
                 id="lichess-username"
                 name={"lichessRef"}
                 ref={lichessRef}
+                defaultValue={Brain.lichessUsername || ""}
                 onKeyDown={(e) => e.stopPropagation()}
                 autoComplete={"on"}
                 placeholder="username"
